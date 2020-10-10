e4201e193e52c4b032733b73d36063d7c74a4fbc
James Dinh <jdinh8124@gmail.com>
2020-10-10 12:59:08 -0700

    completed mod 04

--- 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
+++ 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -8,10 +8,15 @@
         <header class="card-header">
           <p class="card-header-title">heroes list</p>
         </header>
+                <ul class="list is-hoverable">
+          <li v-for="hero in heroes" :key="hero.id">
+            <a class="list-item" @click="selectedHero = hero" :class="{'is-active': selectedHero === hero}"><span>{{hero.firstName}}</span></a> 
+          </li>
+        </ul>
       </div>
     </div>
     <div class="columns">
-      <div class="column is-3">
+      <div class="column is-3" v-if="selectedHero" >
         <header class="card-header">
           <p class="card-header-title">{{ selectedHero.firstName }}</p>
         </header>
@@ -32,6 +37,12 @@
               />
             </div>
             <div class="field">
+              <label for="show" class="checkbox">
+                Show more
+                <input type="checkbox" class="is-primary" id="show" v-model="showMore"/>
+              </label>
+            </div>
+            <div class="field" v-show="showMore">
               <label class="label" for="lastName">last name</label>
               <input
                 class="input"
@@ -39,7 +50,7 @@
                 v-model="selectedHero.lastName"
               />
             </div>
-            <div class="field">
+            <div class="field" v-show="showMore">
               <label class="label" for="description">description</label>
               <input
                 class="input"
@@ -59,12 +70,8 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      selectedHero: {
-        id: 111,
-        firstName: '...',
-        lastName: '...',
-        description: '...',
-      },
+      selectedHero: undefined,
+      showMore: false,
       heroes: [
         {
           id: 10,
